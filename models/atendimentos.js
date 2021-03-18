ce019cbcca4825a57a8a409e7a9bb2946fdb76b8
Luan Oliveira <luanoliveira17@hotmail.com>
2021-03-18 12:49:04 -0300

    Outros verbos http get

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -47,6 +47,31 @@ class Atendimento {
             })
         }        
     }
+
+    lista(res) {
+        const sql = 'SELECT * FROM Atendimentos'
+
+        conexao.query(sql, (erro, resultados) => {
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultados)
+            }
+        })
+    }
+
+    buscaPorId(id, res) {
+        const sql = `SELECT * FROM Atendimentos where id = ${id}`
+
+        conexao.query(sql, (erro, resultado) => {
+            const atendimento = resultado[0]
+            if(erro){
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(atendimento)
+            }
+        })
+    }
 }
 
 module.exports = new Atendimento
