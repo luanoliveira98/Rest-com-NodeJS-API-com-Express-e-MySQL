6337d398389264b0b5a3558adef5cf8083881eac
Luan Oliveira <luanoliveira17@hotmail.com>
2021-03-19 13:07:01 -0300

    Seguranca ao receber dados externos

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,9 +1,18 @@
 const fs = require('fs')
+const path = require('path')
 
 module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
-    const novoCaminho = `./assets/imagens/${nomeDoArquivo}`
+    const tiposValidos = ['jpg', 'png', 'jpeg']
+    const tipo = path.extname(caminho)
+    const tipoEhValido = tiposValidos.indexOf(tipo.substring(1))
+
+    if(tipoEhValido === -1) {
+        console.log('Erro! Tipo inválido')
+    } else {
+        const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`
     
-    fs.createReadStream(caminho)
-    .pipe(fs.createWriteStream(novoCaminho))
-    .on('finish', () => callbackImagemCriada(novoCaminho))
+        fs.createReadStream(caminho)
+        .pipe(fs.createWriteStream(novoCaminho))
+        .on('finish', () => callbackImagemCriada(novoCaminho))
+    }
 }
