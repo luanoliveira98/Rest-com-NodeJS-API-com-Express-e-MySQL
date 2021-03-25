c80583d018fb58147817e86b03fa6033f6593074
Luan Oliveira <luanoliveira17@hotmail.com>
2021-03-24 21:23:10 -0300

    Consumir api com axios

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,5 +1,6 @@
 const moment = require('moment')
 const conexao = require ('../infraestrutura/conexao')
+const axios = require('axios')
 
 class Atendimento {
     adiciona(atendimento, res) {
@@ -63,11 +64,15 @@ class Atendimento {
     buscaPorId(id, res) {
         const sql = `SELECT * FROM Atendimentos where id = ${id}`
 
-        conexao.query(sql, (erro, resultado) => {
+        conexao.query(sql, async (erro, resultado) => {
             const atendimento = resultado[0]
+            const cpf = atendimento.cliente
             if(erro){
                 res.status(400).json(erro)
             } else {
+                const { data } = await axios.get(`http://localhost:8082/${cpf}`)
+                atendimento.cliente = data
+
                 res.status(200).json(atendimento)
             }
         })
